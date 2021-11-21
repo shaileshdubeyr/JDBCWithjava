4223e91fb534052b83202491a506ab666304b4b3
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-22 03:00:50 +0530

    Addeed : code for databasse connection with java

---
+++ src/main/java/com/jdbc/JDBCConnection.java
@@ -0,0 +1,41 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class JDBCConnection {
+
+    public static void main(String[] args) throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll";
+        String userName = "root";
+        String password = "shailesh@96";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath", e);
+
+        }
+        listDrivers();
+        try {
+            System.out.println("Connection to database:" + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successfull !!!!!!!!" + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connection.close();
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" " + driverClass.getClass().getName());
+        }
+    }
+}
